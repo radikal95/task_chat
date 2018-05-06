73df6a84cdf8b863cefcc35d2162566d1164b21e
Arseny Radushin <radushin.arseny@yandex.ru>
2018-05-07 00:08:16 +0300

    eventlet

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from flask import Flask
-from flask_socketio import SocketIO,send
+import eventlet
+eventlet.monkey_patch()
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app)
 
-@socketio.on('messege')
-def handleMessege(msg):
-    print('Messege: ' + msg)
+@app.route('/')
+def index():
+    return render_template('index.html', async_mode=socketio.async_mode)
+
+@socketio.on('message')
+def handleMessage(msg):
+	print('Message: ' + msg)
+	send(msg, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app)
-
+	socketio.run(app)
